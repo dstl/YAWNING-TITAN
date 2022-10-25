92cc0d8643aae774d03ffedc5a150fb6fcab5d59
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-10-25 20:05:07 +0100

    added additional test cases

--- tests/test_repeatable_episodic_output.py
+++ tests/test_repeatable_episodic_output.py
@@ -12,13 +12,10 @@ from yawning_titan.envs.generic.core.action_loops import ActionLoop
 from yawning_titan.envs.generic.core.network_interface import NetworkInterface
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
+import random
 
 
-def test_repeatable_episodic_output():
-    """
-    Test to check that actions undertaken by the red attacking agent
-    are repeatable across all episodes
-    """
+def create_basic_2_agent_loop(episodes:int,seed:int):
     matrix, node_positions = network_creator.create_18_node_network()
 
     entry_nodes = ["0"]
@@ -29,21 +26,63 @@ def test_repeatable_episodic_output():
     red = RedInterface(network_interface)
     blue = BlueInterface(network_interface)
 
-    env = GenericNetworkEnv(red, blue, network_interface, seed=666)
+    env = GenericNetworkEnv(red, blue, network_interface, seed=seed)
 
     check_env(env, warn=True)
 
     _ = env.reset()
 
-    agent = PPO(PPOMlp, env, verbose=1, seed=666) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
+    agent = PPO(PPOMlp, env, verbose=1, seed=seed) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
 
     agent = agent.learn(total_timesteps=1000)
 
 
-    loop = ActionLoop(env,agent,episode_count=2)
+    return ActionLoop(env,agent,episode_count=episodes)
+
+def test_repeatable_episodic_output_set_seed():
+    """
+    Test to check that actions undertaken by the red attacking agent
+    are repeatable across all episodes with a set seed value
+    """
+    
+
+    loop = create_basic_2_agent_loop(episodes=2,seed=666)
 
     results: List[DataFrame] = loop.standard_action_loop() 
 
     print(results[0].compare(results[1]))
     
     assert_frame_equal(results[0],results[1])
+
+
+def test_repeatable_episodic_output_random_seed():
+    """
+    Test to check that actions undertaken by the red attacking agent
+    are repeatable across all episodes with a random seed value
+    """
+    
+
+    loop = create_basic_2_agent_loop(episodes=2,seed=random.randint(1,1000))
+
+    results: List[DataFrame] = loop.standard_action_loop() 
+
+    print(results[0].compare(results[1]))
+    
+    assert_frame_equal(results[0],results[1])
+
+
+def test_repeatable_episodic_output_many_episodes():
+    """
+    Test to check that actions undertaken by the red attacking agent
+    are repeatable across all episodes with a set seed value. ensure 
+    this result is as expected over many episodes
+    """
+    
+
+    loop = create_basic_2_agent_loop(episodes=random.randint(5,12),seed=666)
+
+    results: List[DataFrame] = loop.standard_action_loop() 
+
+    print(results[0].compare(results[1]))
+    
+    assert_frame_equal(results[0],results[-1])
