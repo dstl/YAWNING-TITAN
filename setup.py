333e215b330c3eda3b24475fd05e47b5306fdc05
Chris McCarthy <chris.mccarthy@methods.co.uk>
2022-10-25 12:21:52 +0100

    AIDT-1 - Added dynamic generation of ray[rllib] install string so that the correct ray.3.0.0.dev0 .whl file is used for the given OS and Python version. Fixed issue with path splitting in Windows and updated root-dir name to uppercase to match GitHub repo name. Updated a bunch of dependencies and regenerated the dep_licenses.md file.

--- setup.py
+++ setup.py
@@ -1,28 +1,89 @@
+import sys
+
 from setuptools import find_packages, setup
+
+
+def _ray_3_beta_rllib_py_platform_pip_install() -> str:
+    """
+    Maps the operating system and the Python version to the relevant .whl
+    file for Ray 3.0.0.dev0 beta version. Uses it to build a pip install
+    string for installing Ray 3.0.0.dev0 with the [rllib] extra.
+
+    whl source: https://docs.ray.io/en/master/ray-overview/installation.html
+
+    * A temporary measure to allow for use on Linux, Windows, and MacOS
+    while we wait for ray 3.0.0 release with full Windows support. *
+
+    Returns: A pip install string to install Ray 3.0.0.dev0 with the [rllib]
+        extra for the given OS and Python version.
+    Raises EnvironmentError: When either the operating system is not
+        supported or the Python version is not supported.
+    """
+    ray_wheels_root = "https://s3-us-west-2.amazonaws.com/ray-wheels/latest"
+    ray_3_py_platform_map = {
+        "linux": {  # Linux
+            (3, 8): "/ray-3.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl",
+            (3, 9): "/ray-3.0.0.dev0-cp39-cp39-manylinux2014_x86_64.whl",
+            (3, 10): "/ray-3.0.0.dev0-cp310-cp310-manylinux2014_x86_64.whl",
+        },
+        "darmin": {  # MacOSX
+            (3, 8): "/ray-3.0.0.dev0-cp38-cp38-macosx_10_15_x86_64.whl",
+            (3, 9): "/ray-3.0.0.dev0-cp39-cp39-macosx_10_15_x86_64.whl",
+            (3, 10): "/ray-3.0.0.dev0-cp310-cp310-macosx_10_15_universal2.whl",
+        },
+        "win32": {  # Windows
+            (3, 8): "/ray-3.0.0.dev0-cp38-cp38-win_amd64.whl",
+            (3, 9): "/ray-3.0.0.dev0-cp39-cp39-win_amd64.whl",
+            (3, 10): "/ray-3.0.0.dev0-cp310-cp310-win_amd64.whl",
+        }
+    }
+    py_v = sys.version_info
+    py_v_major_minor = py_v[:2]
+    if sys.platform in ray_3_py_platform_map.keys():
+        if py_v_major_minor in ray_3_py_platform_map[sys.platform].keys():
+            ray_whl = ray_3_py_platform_map[sys.platform][py_v_major_minor]
+            whl_url = f"{ray_wheels_root}{ray_whl}"
+            return f"ray[rllib] @ {whl_url}"
+        else:
+            #  Python version not supported
+            raise EnvironmentError(
+                f"yawningtitan with ray on {sys.platform} is only supported "
+                f"by Python version 3.8.*, 3.9.*, and 3.10.*, currently using "
+                f"python version {py_v.major}.{py_v.minor}.{py_v.micro}"
+            )
+    else:
+        #  OS not supported
+        raise EnvironmentError(
+            f"yawningtitan with ray on {sys.platform} is only supported "
+            f"by Python version 3.8.*, 3.9.*, and 3.10.*, currently using "
+            f"{sys.platform}"
+        )
+
 
 setup(
     name="yawningtitan",
     maintainer="Defence Science and Technology Laboratory UK",
     maintainer_email="oss@dstl.gov.uk",
     url="https://github.com/dstl/YAWNING-TITAN",
-    description="An abstract, flexible and configurable cyber security simulation",
+    description="An abstract, flexible and configurable cyber security "
+                "simulation",
     python_requires=">=3.8",
     version="0.1.0",
     license="MIT",
     install_requires=[
         "gym==0.21.0",
-        "imageio == 2.9.0",
-        "matplotlib == 3.3.4",
-        "networkx == 2.5.1",
-        "numpy == 1.22.0",
-        "ray[rllib]",
-        "scipy == 1.5.4",
+        "imageio==2.9.0",
+        "matplotlib==3.6.1",
+        "networkx==2.5.1",
+        "numpy==1.23.4",
+        _ray_3_beta_rllib_py_platform_pip_install(),
+        "scipy==1.9.2",
         "stable_baselines3",
-        "tabulate == 0.8.9",
+        "tabulate==0.8.9",
         "karateclub",
-        "pandas == 1.2.3",
-        "pyyaml == 5.4.1",
-        "typing-extensions == 3.7.4.3",
+        "pandas==1.3.5",
+        "pyyaml==5.4.1",
+        "typing-extensions==4.0.1",
         "torch",
         "tensorboard",
         "dm-tree",
