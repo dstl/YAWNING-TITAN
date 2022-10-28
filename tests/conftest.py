b55eaa580a83cef4469026ea5b54d8e3d17dd87a
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-10-28 13:18:08 +0100

    made basic 2 agent loop more generic

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy as PPOMlp
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
 
 from yawning_titan.envs.generic.core.blue_interface import BlueInterface
 from yawning_titan.envs.generic.core.red_interface import RedInterface
@@ -16,24 +19,43 @@ from yawning_titan.envs.generic.core.network_interface import NetworkInterface
 def basic_2_agent_loop(request)->ActionLoop:
     matrix, node_positions = network_creator.create_18_node_network()
 
-    entry_nodes = ["0"]
+    entry_nodes = None
+    settings_path = None
+    seed = None
+    num_episodes = 1
 
-    settings_path = Path(__file__).parent / "test_configs/repeatable_threat_config.yaml"
+    if "entry_nodes" in request.param:
+        entry_nodes = request.param["entry_nodes"]
 
-    network_interface = NetworkInterface(matrix, node_positions, entry_nodes=entry_nodes, high_value_target='12',settings_path=settings_path.as_posix())
+    if "settings_file" in request.param:
+        settings_path = (Path(__file__).parent / "test_configs" / request.param["settings_file"]).as_posix()
+
+    if "seed" in request.param:
+        seed = request.param["seed"]
+
+    if "episodes" in request.param:
+        num_episodes = request.param["episodes"]    
+    
+
+    network_interface = NetworkInterface(matrix, node_positions, entry_nodes=entry_nodes, high_value_target='12',settings_path=settings_path)
 
     red = RedInterface(network_interface)
     blue = BlueInterface(network_interface)
 
-    env = GenericNetworkEnv(red, blue, network_interface, seed=request.param["seed"])
+    env = GenericNetworkEnv(red, blue, network_interface, seed=seed)
 
     check_env(env, warn=True)
 
     _ = env.reset()
 
-    agent = PPO(PPOMlp, env, verbose=1, seed=request.param["seed"]) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
+    eval_callback = EvalCallback(
+            Monitor(env), eval_freq=1000, deterministic=False, render=False
+        )
 
-    agent = agent.learn(total_timesteps=1000)
+    agent = PPO(PPOMlp, env, verbose=1, seed=seed) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
 
+    agent.learn(
+            total_timesteps=1000, n_eval_episodes=100, callback=eval_callback
+    )
 
-    return ActionLoop(env,agent,episode_count=request.param["episodes"])
+    return ActionLoop(env,agent,episode_count=num_episodes)
--- tests/test_repeatable_episodic_output.py
+++ tests/test_repeatable_episodic_output.py
@@ -9,9 +9,9 @@ import pytest
 
 @pytest.mark.parametrize("basic_2_agent_loop",
     [
-        {"seed":666,"episodes":2},
-        {"seed":random.randint(1,1000),"episodes":2},
-        {"seed":random.randint(1,1000),"episodes":random.randint(5,12)}
+        {"seed":666,"episodes":2,"entry_nodes":["0"]},
+        {"seed":random.randint(1,1000),"episodes":2,"entry_nodes":["0"]},
+        {"seed":666,"episodes":random.randint(5,12),"entry_nodes":["0"]}
     ], indirect=True
 )
 def test_repeatable_episodic_output_set_seed(basic_2_agent_loop:ActionLoop):
