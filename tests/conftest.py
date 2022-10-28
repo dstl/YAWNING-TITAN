32388e219e31ca927072cbb1e85e476b9cef7540
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-10-28 20:10:24 +0100

    temp

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
-from pathlib import Path
-
+import os
+from tests import TEST_CONFIG_PATH
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy as PPOMlp
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
 
 from yawning_titan.envs.generic.core.blue_interface import BlueInterface
 from yawning_titan.envs.generic.core.red_interface import RedInterface
@@ -16,26 +18,43 @@ from yawning_titan.envs.generic.core.network_interface import NetworkInterface
 def basic_2_agent_loop(request)->ActionLoop:
     matrix, node_positions = network_creator.create_18_node_network()
 
-    entry_nodes = ["0"]
+    entry_nodes = None
     settings_path = None
+    seed = None
+    num_episodes = 1
 
-    if "settings_path" in request.param:
-        settings_path = (Path(__file__).parent / request.param["settings_path"]).as_posix()
+    if "entry_nodes" in request.param:
+        entry_nodes = request.param["entry_nodes"]
 
-    network_interface = NetworkInterface(matrix, node_positions, entry_nodes=entry_nodes, high_value_node='12',settings_path=settings_path)
+    if "settings_file" in request.param:
+        settings_path = os.path.join(TEST_CONFIG_PATH, request.param["settings_file"])
+
+    if "seed" in request.param:
+        seed = request.param["seed"]
+
+    if "episodes" in request.param:
+        num_episodes = request.param["episodes"]    
+    
+
+    network_interface = NetworkInterface(matrix, node_positions, entry_nodes=entry_nodes, high_value_target='12',settings_path=settings_path)
 
     red = RedInterface(network_interface)
     blue = BlueInterface(network_interface)
 
-    env = GenericNetworkEnv(red, blue, network_interface, seed=request.param["seed"])
+    env = GenericNetworkEnv(red, blue, network_interface, seed=seed)
 
     check_env(env, warn=True)
 
     _ = env.reset()
 
-    agent = PPO(PPOMlp, env, verbose=1, seed=request.param["seed"]) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
+    eval_callback = EvalCallback(
+            Monitor(env), eval_freq=1000, deterministic=False, render=False
+        )
 
-    agent = agent.learn(total_timesteps=1000)
+    agent = PPO(PPOMlp, env, verbose=1, seed=seed) #TODO: allow PPO to inherit environment seed. Monkey patch additional feature?
 
+    agent.learn(
+            total_timesteps=1000, n_eval_episodes=100, callback=eval_callback
+    )
 
-    return ActionLoop(env,agent,episode_count=request.param["episodes"],filename="test_output")
+    return ActionLoop(env,agent,episode_count=num_episodes)
