43d9c5508a12f5d86fc31f7c0d32d29cffea5ae1
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-07 20:48:46 +0000

    inital test of Config item property

--- yawning_titan/config/game_config/config_group_class.py
+++ yawning_titan/config/game_config/config_group_class.py
@@ -1,23 +1,54 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Dict, Any
-from collections import namedtuple
+from dataclasses import asdict, dataclass
+from typing import Dict, Any, List, Tuple
 
-ConfigItem = namedtuple("value","description")
+import ruamel.yaml as ry
+
+
+@dataclass
+class ConfigItem:
+    value: Any
+    description: str
+    def __post_init__(self):
+        self.__doc__ = 
+
+def just_value_factory(data: List[Tuple[str, Any]]):
+    return {
+        field: value.value if isinstance(value, ConfigItem) else value
+        for field, value in data
+    }
 @dataclass()
 class ConfigGroupABC(ABC):
     def __setattr__(self, __name: str, __value: Any) -> None:
-        if hasattr(self,__name):
-            attr:ConfigItem = getattr(self,__name)
-            attr = attr._replace(value=__value)
-            setattr(self,__name,attr)
-        elif isinstance(__value,namedtuple):
-            super().__setattr__(__name,attr)
+        print("£",__name,__value)
+        if isinstance(__value,ConfigItem):
+            print(1)
+            super().__setattr__(__name,__value)
+        else:
+            print(2)
+            try:
+                self.__dict__[__name].value = __value
+            except AttributeError:
+                super().__setattr__(__name,__value)
 
     def __getattribute__(self, __name: str) -> Any:
-        if hasattr(self,__name) and isinstance(getattr(self,__name),ConfigItem):
-            return getattr(self,__name).value
-        return super().__getattribute__(__name)
+        print("$",__name,super().__getattribute__(__name))
+        return super().__getattribute__(__name).value
+
+    def to_dict(self,fetch_descriptions=False):
+        if fetch_descriptions:
+            return asdict(self)
+        return asdict(self,dict_factory=just_value_factory)
+
+    def as_commented_yaml(self):
+        config_items = self.to_dict()
+        s = ry.CommentedMap(config_items)
+        key:str
+        val:ConfigItem
+        for key,val in config_items.items():
+            #print("k",self.__dict__[section_name.lower()].__dict__[key].__doc__)
+            s.yaml_set_comment_before_after_key(key,val.description,indent=2)
+        return s
 
     @classmethod
     @abstractmethod
--- yawning_titan/config/game_config/game_mode_config.py
+++ yawning_titan/config/game_config/game_mode_config.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from logging import getLogger
 
 import yaml
@@ -11,7 +11,10 @@ from yawning_titan.config.environment.game_rules_config import GameRulesConfig
 from yawning_titan.config.environment.observation_space_config import ObservationSpaceConfig
 from yawning_titan.config.environment.reset_config import ResetConfig
 from yawning_titan.config.environment.rewards_config import RewardsConfig
+from yawning_titan.config.game_config.config_group_class import ConfigGroupABC
 from yawning_titan.config.game_modes import default_game_mode_path
+
+import ruamel.yaml as ry
 
 _LOGGER = getLogger(__name__)
 
@@ -91,3 +94,50 @@ class GameModeConfig:
             rewards_config=RewardsConfig.create(settings["REWARDS"]),
             output_timestep_data_to_json=settings["MISCELLANEOUS"]["output_timestep_data_to_json"]
         )
+
+    def as_formatted_dict(self):
+        settings_dict = {key: val for key, val in self.__dict__.items() if isinstance(val,ConfigGroupABC)}
+        _settings_dict = {}
+
+        for section_name, section_class in settings_dict.items():
+            section_dict = {key:val for key,val in asdict(section_class).items() if type(val) in [int,str,list,bool]}
+            _settings_dict[section_name.upper()] = section_dict
+        return _settings_dict
+
+    def write_to_file(self, settings_path):
+        self.file_path = settings_path
+        with open(settings_path, 'w') as file:
+            yaml.safe_dump(self.as_formatted_dict(), file)
+
+    def write_to_file_with_comments(self,file_path):
+        
+        
+        settings_dict = self.as_formatted_dict()
+        data = ry.round_trip_load(ry.round_trip_dump(settings_dict))
+        MAPIND = 4
+
+        _yaml = ry.YAML()
+        _yaml.indent(mapping=2)
+
+        with open(file_path,"w") as f:
+            #data = _yaml.load(f)
+            print("DOC",data)
+
+            for section_name, section_config in settings_dict.items():
+                data[section_name].yaml_set_start_comment('after test2', indent=2)
+                s = ry.CommentedMap(section_config)
+                for key,val in section_config.items():
+                    #print("k",self.__dict__[section_name.lower()].__dict__[key].__doc__)
+                    s.yaml_set_comment_before_after_key(key,"test comment",indent=2)
+                data[section_name] = s
+                    
+            print(data)
+            _yaml.dump(data,f)
+
+c = GameModeConfig.create("D:/Pycharm projects/YAWNING-TITAN-DEV/YAWNING-TITAN/tests/test_configs/base_config.yaml")
+# obs_compromised_status_config = {
+#     "obs_compromised_status":False,
+#     "obs_node_vuln_status":False,
+#     "obs_node_connections"
+# }
+print(c.observation_space_config.obs_compromised_status)
