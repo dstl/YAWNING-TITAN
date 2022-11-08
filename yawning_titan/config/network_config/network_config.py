9f11148b47ce3fcd8f5e4c25885e5c8b5ed8b81e
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-08 15:52:28 +0000

    code cleanup

--- yawning_titan/config/network_config/network_config.py
+++ yawning_titan/config/network_config/network_config.py
@@ -15,16 +15,22 @@ class NetworkConfig(ConfigGroupABC):
     Class that validates and stores Network Configuration
     """
 
-    matrix: np.array = field(metadata="Stores the matrix dictating how each node is connected to each other")
+    matrix: np.array = field(
+        metadata="Stores the matrix dictating how each node is connected to each other"
+    )
     """Stores the matrix dictating how each node is connected to each other"""
 
-    positions: Dict = field(metadata="Dictionary containing the positions of the nodes in the network (when displayed as a graph)")
+    positions: Dict = field(
+        metadata="Dictionary containing the positions of the nodes in the network (when displayed as a graph)"
+    )
     """Dictionary containing the positions of the nodes in the network (when displayed as a graph)"""
 
     entry_nodes: Optional[List[str]] = field(metadata="List of entry nodes")
     """List of entry nodes"""
 
-    vulnerabilities: Optional[Dict] = field(metadata="Dictionary containing the vulnerabilities of the nodes")
+    vulnerabilities: Optional[Dict] = field(
+        metadata="Dictionary containing the vulnerabilities of the nodes"
+    )
     """Dictionary containing the vulnerabilities of the nodes"""
 
     high_value_targets: Optional[List[str]] = field(metadata="List of high value nodes")
@@ -32,19 +38,19 @@ class NetworkConfig(ConfigGroupABC):
 
     @classmethod
     def create(
-            cls,
-            matrix: np.array,
-            positions: Dict,
-            entry_nodes: Optional[List[str]] = None,
-            vulnerabilities: Optional[Dict] = None,
-            high_value_targets: Optional[List[str]] = None
+        cls,
+        matrix: np.array,
+        positions: Dict,
+        entry_nodes: Optional[List[str]] = None,
+        vulnerabilities: Optional[Dict] = None,
+        high_value_targets: Optional[List[str]] = None,
     ):
         cls._validate(
             matrix=matrix,
             positions=positions,
             entry_nodes=entry_nodes,
             vulnerabilities=vulnerabilities,
-            high_value_targets=high_value_targets
+            high_value_targets=high_value_targets,
         )
 
         network_config = NetworkConfig(
@@ -52,21 +58,22 @@ class NetworkConfig(ConfigGroupABC):
             positions=positions,
             entry_nodes=entry_nodes,
             vulnerabilities=vulnerabilities,
-            high_value_targets=high_value_targets
+            high_value_targets=high_value_targets,
         )
 
         return network_config
 
     @classmethod
     def _validate(
-            cls,
-            matrix: np.array,
-            positions: Dict,
-            entry_nodes: Optional[List[str]] = None,
-            vulnerabilities: Optional[Dict] = None,
-            high_value_targets: Optional[List[str]] = None
+        cls,
+        matrix: np.array,
+        positions: Dict,
+        entry_nodes: Optional[List[str]] = None,
+        vulnerabilities: Optional[Dict] = None,
+        high_value_targets: Optional[List[str]] = None,
     ):
         # check that no entry nodes and high value nodes intersect
         if set(entry_nodes) & set(high_value_targets):
             warnings.warn(
-                "Provided entry nodes and high value targets intersect and may cause the training to prematurely end")
+                "Provided entry nodes and high value targets intersect and may cause the training to prematurely end"
+            )
