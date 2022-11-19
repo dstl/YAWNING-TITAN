d7adcfddaabaac62040f46cdfb17f95fd2ce0c41
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-19 17:42:47 +0000

    test

--- yawning_titan_gui/management/commands/setup.py
+++ yawning_titan_gui/management/commands/setup.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import shutil
 from django.core.management.base import BaseCommand
 class Command(BaseCommand):
@@ -7,7 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print("Running setup...")
         from yawning_titan import DATA_DIR
-        from yawning_titan_gui import _YT_FRONT_ROOT_DIR        
+        from yawning_titan_gui import _YT_GUI_ROOT_DIR        
         from dirs import _create_app_dirs,_copy_package_data_notebooks_to_notebooks_dir
 
         _create_app_dirs()
@@ -15,7 +14,7 @@ class Command(BaseCommand):
         
         # Creates the static ui files copy in the data directory
         shutil.copytree(
-            (_YT_FRONT_ROOT_DIR / "static").as_posix(), 
+            (_YT_GUI_ROOT_DIR / "static").as_posix(), 
             DATA_DIR.as_posix(), 
             dirs_exist_ok=True
         )
