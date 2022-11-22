ba82004ccdbce383081779192e140405ef6d19b8
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-22 12:00:01 +0000

    temp

--- yawning_titan_gui/management/commands/setup.py
+++ yawning_titan_gui/management/commands/setup.py
@@ -1,7 +1,8 @@
 import shutil
 from django.core.management.base import BaseCommand
 
-from setup import _create_app_dirs
+import sys
+#from setup import _create_app_dirs
 class Command(BaseCommand):
     help = 'sets up yawning titan dependencies'
 
@@ -9,14 +10,15 @@ class Command(BaseCommand):
         print("Running setup...")
         from yawning_titan import DATA_DIR
         from yawning_titan_gui import _YT_GUI_ROOT_DIR        
-    
 
-        _create_app_dirs()
+        #import setup
+        # print(sys.path, setup._create_app_dirs)
+        #_create_app_dirs()
         #_copy_package_data_notebooks_to_notebooks_dir()
         
         # Creates the static ui files copy in the data directory
-        # shutil.copytree(
-        #     (_YT_GUI_ROOT_DIR / "static").as_posix(), 
-        #     DATA_DIR.as_posix(), 
-        #     dirs_exist_ok=True
-        # )
+        shutil.copytree(
+            (_YT_GUI_ROOT_DIR / "static").as_posix(), 
+            DATA_DIR.as_posix(), 
+            dirs_exist_ok=True
+        )
