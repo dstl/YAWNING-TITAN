a2134a43103cc381f769eafe38e31c10587cd145
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-22 11:49:07 +0000

    AIDT-105 modified pyinstaller requirment to reference pyinstaller develop branch

--- yawning_titan_gui/management/commands/setup.py
+++ yawning_titan_gui/management/commands/setup.py
@@ -1,5 +1,7 @@
 import shutil
 from django.core.management.base import BaseCommand
+
+from setup import _create_app_dirs
 class Command(BaseCommand):
     help = 'sets up yawning titan dependencies'
 
@@ -7,14 +9,14 @@ class Command(BaseCommand):
         print("Running setup...")
         from yawning_titan import DATA_DIR
         from yawning_titan_gui import _YT_GUI_ROOT_DIR        
-        from setup import _create_app_dirs,_copy_package_data_notebooks_to_notebooks_dir
+    
 
         _create_app_dirs()
-        _copy_package_data_notebooks_to_notebooks_dir()
+        #_copy_package_data_notebooks_to_notebooks_dir()
         
         # Creates the static ui files copy in the data directory
-        shutil.copytree(
-            (_YT_GUI_ROOT_DIR / "static").as_posix(), 
-            DATA_DIR.as_posix(), 
-            dirs_exist_ok=True
-        )
+        # shutil.copytree(
+        #     (_YT_GUI_ROOT_DIR / "static").as_posix(), 
+        #     DATA_DIR.as_posix(), 
+        #     dirs_exist_ok=True
+        # )
