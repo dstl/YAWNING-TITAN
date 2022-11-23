37efd5b33a7d0b281085bd224c4b3bbb1ee83619
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-23 18:17:57 +0000

    AIDT-105 run precommits

--- yawning_titan_gui/templatetags/tags.py
+++ yawning_titan_gui/templatetags/tags.py
@@ -1,22 +1,34 @@
-from django import template
-from django.utils.safestring import mark_safe
-from django.urls import reverse
 import json
+
+from django import template
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-#basic helper tags
+# basic helper tags
+
 
 @register.filter(is_safe=True)
 def js(obj):
+    """Return argument in javascript markup."""
     return mark_safe(json.dumps(obj))
 
-@register.filter
-def to_id(value:str):
-    return value.replace(" ","-")
 
 @register.filter
-def get_url(url_name:str):
+def to_id(value: str):
+    """Replaces spaces with dashes in string argument to form html formatted id."""
+    return value.replace(" ", "-")
+
+
+@register.filter
+def get_url(url_name: str):
+    """
+    Wrapped implementation of Django's reverse url.
+
+    A lookup that returns the url by name
+    or empty string when the url does not exist.
+    """
     try:
         return reverse(url_name)
     except Exception:
