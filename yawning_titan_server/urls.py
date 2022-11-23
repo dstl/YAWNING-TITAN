376dbc4dd4a776b52a32fd797baa3475ff9eef98
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-23 18:17:57 +0000

    AIDT-105 run precommits

--- yawning_titan_server/urls.py
+++ yawning_titan_server/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from yawning_titan_gui.views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
+from yawning_titan_gui.views import home
 
 urlpatterns = [
-    path('', home.as_view()),
+    path("", home.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
