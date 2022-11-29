6b5d0a41e4eb11baa880750e0f9b78da4f8c5baa
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-29 13:37:47 +0000

    AIDT-107 stripped out AIDT-108 functionality

--- yawning_titan_gui/static/js/game_mode.js
+++ yawning_titan_gui/static/js/game_mode.js
@@ -7,107 +7,6 @@ $(document).ready(function(){
         $(".game-mode").find(".subhead:not(:contains(" + $(this).val() + "))").closest(".game-mode").addClass("hidden");
     });
 
-    $(".game-mode.selectable").click(function(){
-        if($(this).hasClass("selected")){
-            $(this).removeClass("selected");
-            $("#game-mode-set").removeClass("complete");
-            update_tooltip("#game-mode-set","complete","incomplete");
-        }else{
-            $(".game-mode").removeClass("selected");
-            $(this).addClass("selected");
-            $("#game-mode-set").addClass("complete");
-            update_tooltip("#game-mode-set","incomplete","complete");
-        }
-    });
-    // check select dependencies on page load
-    $("select").each(function(){
-        $("."+$("option:selected",this).text().replaceAll(" ","_")).removeClass("hidden")
-    });
-
-    $(".form-check-input").each(function(){
-        if($(this).is(":checked")){
-            $(this).addClass("checked");
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).removeClass("hidden");
-        }
-    });
-
-    $(".grouped.parent").each(function(){
-        console.log("$$",$(this).val());
-        if($(this).val().length > 0 & $(this).val() != "on"){
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).removeClass("hidden")
-        }
-    });
-
-    // update select dependencies
-    $(document).on("change","select",function(){
-        $(".grouped").addClass("hidden");
-        el =  $("."+$("option:selected",this).text().replaceAll(" ","_"));
-        el.removeClass("hidden");
-    });
-
-    // update checkbox dependencies
-    $(document).on("change",".form-check-input",function(){
-        if($(this).is(":checked")){
-            $(this).addClass("checked");
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).removeClass("hidden");
-        }else{
-            $(this).removeClass("checked");
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).addClass("hidden");
-        }
-    });
-
-    // update input dependencies
-    $(document).on("keyup",".grouped.parent",function(){
-        if($(this).val().length > 0){
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).removeClass("hidden")
-        }else{
-            $(`.${$(this).get(0).classList[1]}:not(.parent)`).addClass("hidden")
-        }
-    });
-
-    // wrapper for async post request for config section form processing
-    function submit_form(form_element){
-        config = new FormData($(form_element)[0]);
-        config.append('form_name',$(form_element).data("form-name"));
-        return $.ajax({
-            type: "POST",
-            url: window.location.href,
-            data: config,
-            processData: false,
-            contentType: false,
-            cache: false,
-            dataType: "json"
-        });
-    }
-
-    // ajax post request and attached reload callback
-    function submit_game_mode(game_mode_name,operation,additional_params={}){
-        if (game_mode_name.length > 0){
-            $.ajax({
-                type: "POST",
-                url: "/manage_config/",
-                data: Object.assign({},{"game_mode_name":game_mode_name,"operation":operation},additional_params),
-            }).done(function(){
-                location.reload();
-            });
-        }
-    }
-
-    $("#config-form-icons>.icon").click(function(){
-        $("#config-form-icons>.icon").removeClass("selected")
-        $(this).addClass("selected");
-        $(".form-container").addClass("hidden");
-        console.log("TT",$(this).data("form"));
-        $($(this).data("form")).parent().removeClass("hidden");
-    });
-
-    // button actions
-    $("#game-config-submit").click(function(){
-        $(".config-form").each(function(){
-            submit_form(this);
-        });
-    });
-
     // dialogue launchers
     $("#create-game-mode").click(function(){
         toggle_dialogue("#create-dialogue");
@@ -144,8 +43,4 @@ $(document).ready(function(){
     $("#delete-dialogue .submit").click(function(){
         submit_game_mode(selected_game_mode, "delete");
     });
-
-
-
-
 });
--- yawning_titan_server/urls.py
+++ yawning_titan_server/urls.py
@@ -2,26 +2,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from yawning_titan_gui.views import (
-    GameModeConfigView,
-    GameModesView,
-    HomeView,
-    config_file_manager,
-)
+from yawning_titan_gui.views import GameModesView, HomeView, config_file_manager
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("game_modes/", GameModesView.as_view(), name="Manage game modes"),
+    # TODO: replace below HomeView's with view object for game mode config
     path(
         "game_mode_config/<str:game_mode_file>/",
-        GameModeConfigView.as_view(),
+        HomeView.as_view(),
         name="game mode config",
     ),
     path(
         "game_mode_config/<str:game_mode_file>/<str:section>/",
-        GameModeConfigView.as_view(),
+        HomeView.as_view(),
         name="game mode config",
     ),
-    path("game_mode_config/", GameModeConfigView.as_view(), name="game mode config"),
+    path("game_mode_config/", HomeView.as_view(), name="game mode config"),
     path("manage_config/", config_file_manager, name="manage config"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
