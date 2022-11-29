158b8336e728ce97ad0e2527b2540a08e0349b32
Rumbelows <96006840+Rumbelows@users.noreply.github.com>
2022-11-29 13:20:13 +0000

    Revert "AIDT-106 build the yt gui page template"

--- yawning_titan_server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-
-from yawning_titan_gui.views import home
-
-urlpatterns = [
-    path("", home.as_view()),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
