1bea2bb22937718a200daa71804cbe52f787b5ef
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-11-30 10:15:07 +0000

    AIDT-106 added app.js docstring

--- yawning_titan_gui/static/js/app.js
+++ yawning_titan_gui/static/js/app.js
@@ -4,15 +4,16 @@ function csrfSafeMethod(method) {
 }
 
 $(document).ready(function(){
+    //open sandwich sidebar
     $('#sandwich-icon').click(function(){
 		$(this).toggleClass('open');
         $($(this).data("sidebar")).toggleClass('open');
 	});
-
+    //close center dialogue
     $(".dialogue-center .cancel").click(function(){
         toggle_dialogue($(this).closest(".dialogue-center"))
     });
-    
+    //pass protected token with request
     $.ajaxSetup({
         beforeSend: function(xhr, settings) {
             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
