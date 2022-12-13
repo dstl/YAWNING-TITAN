7c85828c4babd7d411e6acb5168a1a48c9b63c25
Stefen Lamothe <rumbelowsservices@gmail.com>
2022-12-13 19:21:44 +0000

    AIDT-108 corrected handling of default_game_mode

--- yawning_titan_gui/__init__.py
+++ yawning_titan_gui/__init__.py
@@ -2,4 +2,5 @@ from pathlib import Path
 from typing import Final
 
 _YT_GUI_ROOT_DIR: Final[Path] = Path(__file__).parent.resolve()
-DEFAULT_GAME_MODE: Final[Path] = "everything_off_config.yaml"
+DEFAULT_GAME_MODE: Final[str] = "everything_off_config.yaml"
+"""The filename of the game mode file to act as the base for all game modes """
