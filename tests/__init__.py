45f62b01fa9ef0b6f72e132f842d1546607ebf56
Stefen Lamothe <rumbelowsservices@gmail.com>
2023-01-19 10:08:51 +0000

    AIDT-144 reconfigured old/new config file stucture for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,13 +2,21 @@ import os
 from pathlib import Path
 from typing import Final
 
-TEST_CONFIG_PATH: Final[Path] = Path(
-    os.path.join(Path(__file__).parent.resolve(), "test_configs", "game_mode")
+TEST_CONFIG_PATH_OLD: Final[Path] = Path(
+    os.path.join(Path(__file__).parent.resolve(), "test_configs", "game_mode", "old")
+)
+
+TEST_CONFIG_PATH_NEW: Final[Path] = Path(
+    os.path.join(Path(__file__).parent.resolve(), "test_configs", "game_mode", "new")
 )
 
 TEST_BASE_CONFIG_PATH = Path(
     os.path.join(
-        Path(__file__).parent.resolve(), "test_configs", "game_mode", "base_config.yaml"
+        Path(__file__).parent.resolve(),
+        "test_configs",
+        "game_mode",
+        "old",
+        "base_config.yaml",
     )
 )
 
