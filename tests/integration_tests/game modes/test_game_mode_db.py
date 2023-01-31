5b76c1973645ffc31324e5bf837b1e937003c66f
Stefen Lamothe <rumbelowsservices@gmail.com>
2023-01-31 20:39:09 +0000

    AIDT-203 added game mode db

--- tests/integration_tests/game modes/test_game_mode_db.py
+++ tests/integration_tests/game modes/test_game_mode_db.py
@@ -1,4 +1,4 @@
-"""Test the main :class:`` class."""
+"""Test the main :class: `~yawning_titan.game_modes.game_mode_db.GameModeDB` class."""
 import os
 from unittest.mock import patch
 
@@ -8,7 +8,8 @@ from tests.mock_and_patch.yawning_titan_db_patch import yawning_titan_db_init_pa
 from yawning_titan.db.doc_metadata import DocMetadataSchema
 from yawning_titan.db.yawning_titan_db import YawningTitanDB
 from yawning_titan.exceptions import YawningTitanDBError
-from yawning_titan.game_modes.game_mode_db import GameModeDB
+from yawning_titan.game_modes.game_mode_db import GameModeDB, GameModeSchema
+from yawning_titan.networks.network_db import default_18_node_network
 
 
 @pytest.mark.integration_test
@@ -29,5 +30,68 @@ def test_delete_default_game_mode_delete_fails():
 
         with pytest.raises(YawningTitanDBError):
             db.remove(config)
+
+        db._db.close_and_delete_temp_db()
+
+
+@pytest.mark.integration_test
+def test_game_mode_configuration_query():
+    """Test searching by game mode property returns appropriate result."""
+    with patch.object(YawningTitanDB, "__init__", yawning_titan_db_init_patch):
+        db = GameModeDB()
+        found = db.search(
+            GameModeSchema.CONFIGURATION.red.agent_attack.ignores_defences == False
+        )
+        not_found = db.search(
+            GameModeSchema.CONFIGURATION.red.agent_attack.ignores_defences == "1"
+        )
+
+        assert len(not_found) == 0
+        assert len(found) == 6
+        db._db.close_and_delete_temp_db()
+
+
+@pytest.mark.integration_test
+def test_game_mode_compatibility_query_network():
+    """Test searching by network element compatibility returns appropriate result."""
+    with patch.object(YawningTitanDB, "__init__", yawning_titan_db_init_patch):
+        db = GameModeDB()
+
+        network = default_18_node_network()
+
+        # all are compatible as they are either unrestricted or sufficiently sized.
+        found = db.search(GameModeSchema.ENTRY_NODES.works_with(network))
+        assert len(found) == 6
+
+        # all but 1 are compatible as they are unrestricted however default game mode does not work with networks with no entry nodes.
+        found = db.search(GameModeSchema.HIGH_VALUE_NODES.works_with(network))
+        assert len(found) == 5
+
+        found = db.search(GameModeSchema.NETWORK_NODES.works_with(network))
+        assert len(found) == 5
+
+        # default game mode incompatible with network as high value nodes and network nodes have incompatible counts
+        found = db.search(GameModeSchema.NETWORK_COMPATIBILITY.compatible_with(network))
+        assert len(found) == 5
+
+        db._db.close_and_delete_temp_db()
+
+
+@pytest.mark.integration_test
+def test_game_mode_compatibility_query_integer():
+    """Test searching by network element compatibility expressed as an integer returns appropriate result."""
+    with patch.object(YawningTitanDB, "__init__", yawning_titan_db_init_patch):
+        db = GameModeDB()
+
+        # all are compatible as they are either unrestricted or sufficiently sized.
+        found = db.search(GameModeSchema.ENTRY_NODES.works_with(1))
+        assert len(found) == 6
+
+        # all but 1 are compatible as they are unrestricted however default game mode does not work with networks with no entry nodes.
+        found = db.search(GameModeSchema.HIGH_VALUE_NODES.works_with(0))
+        assert len(found) == 5
+
+        found = db.search(GameModeSchema.NETWORK_NODES.works_with(18))
+        assert len(found) == 5
 
         db._db.close_and_delete_temp_db()
--- yawning_titan/db/query.py
+++ yawning_titan/db/query.py
@@ -1,89 +1,8 @@
 """The :mod:`~yawning_titan.db.query` module provides a Yawning-Titan extension to :class:`tinydb.queries.Query`."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
-
 from tinydb import Query
 from tinydb.queries import QueryInstance
-
-if TYPE_CHECKING:
-    from yawning_titan.networks.network import Network
-
-
-class CompatibilityQuery(Query):
-    """
-    The :class:`~yawning_titan.db.query.YawningTitanQuery` class extends :class:`tinydb.queries.Query`.
-
-    Extended to provide common pre-defined test functions that call :func:`tinydb.queries.Query.test`, These functions
-    are specific to the compatibility of :class: `~yawning_titan.game_modes.game_mode.GameMode`
-    and :class: `~yawning_titan.networks.network.Network` objects.
-    """
-
-    def works_with(self, n: int, include_null: Optional[bool] = True) -> QueryInstance:
-        """Tests the game mode can work with a network with the parameter with a value of ``n``.
-
-        Fields whose value is either unrestricted or where ``n`` is in the specified range are returned in the search.
-
-        :Example:
-
-        >>> from yawning_titan.game_modes.game_mode_db import GameModeDB
-        >>> from yawning_titan.db.query import YawningTitanQuery
-        >>> db = GameModeDB()
-        >>> db.search(YawningTitanQuery.ENTRY_NODES.works_with(18)))
-
-        :param n: The target value of a field as an int.
-        :param include_null: Whether to include fields where part of the range is unbounded.
-        :return: ``True`` if it does exist, otherwise ``False``.
-        """
-
-        def test_works_with(val, n, include_null):
-            return check_element(val, n, include_null)
-
-        return self.test(test_works_with, n, include_null)
-
-    def compatible_with(self, n, include_null: Optional[bool] = True) -> QueryInstance:
-        """Tests the game mode can work with a specified network ``n``.
-
-        Fields where all network parameter restrictions are satisfied or unrestricted are returned in the search.
-
-
-        :Example:
-
-        >>> from yawning_titan.game_modes.game_mode_db import GameModeDB
-        >>> from yawning_titan.db.query import YawningTitanQuery
-        >>> db = GameModeDB()
-        >>> db.search(YawningTitanQuery.GAME_MODE.compatible_with(network)))
-
-        :param n: The target value of a field as a Network.
-        :param include_null: Whether to include fields where part of the range is unbounded.
-        :return: ``True`` if it does exist, otherwise ``False``.
-        """
-
-        def test_compatible_with(val: dict, n: Network, include_null):
-            if (
-                not isinstance(val, dict)
-                or not isinstance(n, Network)
-                or not all(
-                    k in val
-                    for k in ["entry_node_count", "high_value_node_count", "node_count"]
-                )
-            ):
-                return False
-            mapper = {
-                "entry_node_count": len(n.entry_nodes.nodes)
-                if n.entry_nodes.nodes
-                else n.entry_nodes.random_placement.count,
-                "high_value_node_count": len(n.high_value_nodes.nodes)
-                if n.high_value_nodes.nodes
-                else n.high_value_nodes.random_placement.count,
-                "node_count": len(n.matrix),
-            }
-            results = [
-                check_element(e, mapper[k], include_null) for k, e in val.items()
-            ]
-            return all(results)
-
-        return self.test(test_compatible_with, n, include_null)
 
 
 class YawningTitanQuery(Query):
@@ -226,37 +145,3 @@ class YawningTitanQuery(Query):
                 return False
 
         return self.test(test_len, i)
-
-
-def check_element(el_dict, n, include_null):
-    """Check that a restrict-able element is suitable for a given value.
-
-    I.e the value lies in a given range.
-
-    :param el_dict: The dictionary representing of a ConfigGroup related to a range restricted element
-    :param n: The target value of a field as a Network.
-    :param include_null: Whether to include fields where part of the range is unbounded.
-
-    :return: A boolean representing of the element is suited for a particular value.
-    """
-    if not isinstance(el_dict, dict) or not all(
-        k in el_dict for k in ["min", "max", "restrict"]
-    ):
-        return False
-
-    if not el_dict["restrict"]:
-        return True
-
-    check_min = False
-    if el_dict["min"] is None and include_null:
-        check_min = True
-    elif n > el_dict["min"]:
-        check_min = True
-
-    check_max = False
-    if el_dict["max"] is None and include_null:
-        check_max = True
-    elif n < el_dict["max"]:
-        check_max = True
-
-    return check_min and check_max
