c5331fa1cfed6ddd785cae272f7abacd5bc73efd
Chris McCarthy <chris.mccarthy@methods.co.uk>
2023-03-01 14:59:14 +0000

    AIDT-254 - Removed key_by_name from Network.get_nodes in network.py. - Refactored BlueInterface.perform_action in blue_interface.py so that is only gets and uses the node lst rather than the dict.

--- yawning_titan/envs/generic/core/blue_interface.py
+++ yawning_titan/envs/generic/core/blue_interface.py
@@ -122,13 +122,10 @@ class BlueInterface(BlueActionSet):
                 ):
                     blue_action, blue_node = self.do_nothing()
                 else:
-                    node_name_dict = self.network_interface.current_graph.get_nodes(
-                        key_by_name=True
-                    )
+                    node_name_list = self.network_interface.current_graph.get_nodes()
 
-                    action_node = sorted(list(node_name_dict.values()))[
-                        action_node_number
-                    ]
+                    action_node = sorted(node_name_list)[action_node_number]
+
                     action_taken = int(action % self.number_of_actions)
 
                     blue_action, blue_node = self.action_dict[action_taken](action_node)
