2ae7fc6d3c1c4e173e716234e2e176b90c8996f6
Stefen Lamothe <rumbelowsservices@gmail.com>
2023-03-03 13:17:32 +0000

    fixed issue where nodes with no name break graph rendering and action choices

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,57 @@ from yawning_titan.networks.network import (
     RandomHighValueNodePreference,
 )
 from yawning_titan.networks.network_db import default_18_node_network
+from yawning_titan.networks.node import Node
 from yawning_titan.yawning_titan_run import YawningTitanRun
+
+
+@pytest.fixture()
+def corporate_network() -> Network:
+    """An example network with components akin to that of a basic corporate network."""
+    router_1 = Node()
+    switch_1 = Node()
+    switch_2 = Node()
+    pc_1 = Node()
+    pc_2 = Node()
+    pc_3 = Node()
+    pc_4 = Node()
+    pc_5 = Node()
+    pc_6 = Node()
+    server_1 = Node()
+    server_2 = Node()
+    network = Network(
+        set_random_entry_nodes=True,
+        num_of_random_entry_nodes=3,
+        set_random_high_value_nodes=True,
+        num_of_random_high_value_nodes=3,
+        set_random_vulnerabilities=True,
+    )
+    network.add_node(router_1)
+    network.add_node(switch_1)
+    network.add_node(switch_2)
+    network.add_node(pc_1)
+    network.add_node(pc_2)
+    network.add_node(pc_3)
+    network.add_node(pc_4)
+    network.add_node(pc_5)
+    network.add_node(pc_6)
+    network.add_node(server_1)
+    network.add_node(server_2)
+    network.add_edge(router_1, switch_1)
+    network.add_edge(switch_1, server_1)
+    network.add_edge(switch_1, pc_1)
+    network.add_edge(switch_1, pc_2)
+    network.add_edge(switch_1, pc_3)
+    network.add_edge(router_1, switch_2)
+    network.add_edge(switch_2, server_2)
+    network.add_edge(switch_2, pc_4)
+    network.add_edge(switch_2, pc_5)
+    network.add_edge(switch_2, pc_6)
+    network.reset_random_entry_nodes()
+    network.reset_random_high_value_nodes()
+    network.reset_random_vulnerabilities()
+    network.set_node_positions()
+    return network
 
 
 @pytest.fixture(scope="session")
