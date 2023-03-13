09223246b3eedeb0a6efe8b80fc608597b2ddb86
Stefen Lamothe <rumbelowsservices@gmail.com>
2023-03-13 09:37:55 +0000

    AIDT-204 added yawning titan run to gui

--- yawning_titan_gui/forms/run_form.py
+++ yawning_titan_gui/forms/run_form.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 from django import forms as django_forms
 from django.forms import widgets
 
@@ -11,15 +9,23 @@ class RunForm(django_forms.Form):
 
     deterministic = django_forms.BooleanField(
         widget=widgets.CheckboxInput(
-            attrs={"role": "switch", "class": "form-check-input"}
+            attrs={"role": "switch", "class": "inline form-check-input"}
         ),
         required=False,
         label="Deterministic",
         help_text="Whether the evaluation should use stochastic or deterministic actions",
     )
+    collect_additional_per_ts_data = django_forms.BooleanField(
+        widget=widgets.CheckboxInput(
+            attrs={"role": "switch", "class": "inline form-check-input"}
+        ),
+        required=False,
+        label="Collect additional data",
+        help_text="Whether to collect additional data for each timestep of the game",
+    )
     save = django_forms.BooleanField(
         widget=widgets.CheckboxInput(
-            attrs={"role": "switch", "class": "form-check-input"}
+            attrs={"role": "switch", "class": "inline form-check-input"}
         ),
         required=False,
         label="Save trained agent",
@@ -27,49 +33,49 @@ class RunForm(django_forms.Form):
     )
     export = django_forms.BooleanField(
         widget=widgets.CheckboxInput(
-            attrs={"role": "switch", "class": "form-check-input"}
+            attrs={"role": "switch", "class": "inline form-check-input"}
         ),
         required=False,
         label="Export run",
         help_text="Export the YawningTitanRun as a zip.",
     )
     total_timesteps = django_forms.IntegerField(
-        widget=widgets.NumberInput(attrs={"class": "form-control"}),
+        widget=widgets.NumberInput(attrs={"class": "inline form-control"}),
         required=False,
         help_text="The number of samples (env steps) to train on",
         label="Total timesteps",
     )
     training_runs = django_forms.IntegerField(
-        widget=widgets.NumberInput(attrs={"class": "form-control"}),
+        widget=widgets.NumberInput(attrs={"class": "inline form-control"}),
         required=False,
         help_text="The number of times the agent is trained",
         label="Training runs",
     )
     n_eval_episodes = django_forms.IntegerField(
-        widget=widgets.NumberInput(attrs={"class": "form-control"}),
+        widget=widgets.NumberInput(attrs={"class": "inline form-control"}),
         required=False,
         help_text="The number of episodes to evaluate the agent",
         label="N eval episodes",
     )
+
     verbose = django_forms.FloatField(
-        widget=RangeInput(attrs={"class": "form-control", "step": "1"}),
+        widget=RangeInput(attrs={"class": "inline form-control", "step": "1"}),
         required=False,
         help_text="The verbosity level of the logged output",
         min_value=0,
         max_value=2,
         label="Verbosity level",
     )
-
     render = django_forms.BooleanField(
         widget=widgets.CheckboxInput(
-            attrs={"role": "switch", "class": "form-check-input"}
+            attrs={"role": "switch", "class": "inline form-check-input"}
         ),
         required=False,
         label="Render gif",
         help_text="Whether the output should render a gif action loop",
     )
     num_episodes = django_forms.IntegerField(
-        widget=widgets.NumberInput(attrs={"class": "form-control"}),
+        widget=widgets.NumberInput(attrs={"class": "inline form-control"}),
         required=False,
         help_text="The number of episodes to run",
         label="Number of episodes",
@@ -90,13 +96,28 @@ class RunForm(django_forms.Form):
 
     @property
     def evaluation_fields(self):
-        return {k: v for k, v in self.fields.items() if k in ["render", "num_episodes"]}
+        """The fields of the form related to evaluation."""
+        return [
+            f
+            for f in self
+            if f.id_for_label.replace("id_", "")
+            in ["render", "num_episodes", "verbose"]
+        ]
 
     @property
     def training_fields(self):
-        return {
-            k: v
-            for k, v in self.fields.items()
-            if k
-            in ["deterministic", "total_timesteps", "training_runs", "n_eval_episodes"]
-        }
+        """The fields of the form related to agent training."""
+        return [
+            f
+            for f in self
+            if f.id_for_label.replace("id_", "")
+            in [
+                "deterministic",
+                "collect_additional_per_ts_data",
+                "save",
+                "export",
+                "total_timesteps",
+                "training_runs",
+                "n_eval_episodes",
+            ]
+        ]
