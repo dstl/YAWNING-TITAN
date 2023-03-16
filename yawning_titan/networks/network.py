172e8c11f8dc180dd286249c263fb995ac0f793c
Czar Echavez <Czar.Echavez@envitia.com>
2023-03-16 15:36:10 +0000

    AIDT-184: fixing the network tabs + commenting out the set_node_positions

--- yawning_titan/networks/network.py
+++ yawning_titan/networks/network.py
@@ -235,7 +235,7 @@ class Network(nx.Graph):
             super().add_node(node_for_adding, **kwargs)
             if node_for_adding.entry_node or node_for_adding.high_value_node:
                 self._check_intersect(node_for_adding)
-            self.set_node_positions()
+            # self.set_node_positions()
 
     def remove_node(self, n: Node):
         """
@@ -244,7 +244,7 @@ class Network(nx.Graph):
         Extend the `remove_node` method of the superclass.
         """
         super().remove_node(n)
-        self.set_node_positions()
+        # self.set_node_positions()
 
     def add_edge(self, u_of_edge: Node, v_of_edge: Node, **kwargs):
         """
@@ -253,7 +253,7 @@ class Network(nx.Graph):
         Extend the `add_edge` method of the superclass.
         """
         super().add_edge(u_of_edge, v_of_edge, **kwargs)
-        self.set_node_positions()
+        # self.set_node_positions()
 
     def remove_edge(self, u: Node, v: Node):
         """
@@ -461,7 +461,7 @@ class Network(nx.Graph):
                 remove_existing=remove_existing_edges,
             )
 
-        self.set_node_positions()
+        # self.set_node_positions()
 
         for k, v in config_dict.items():
             if hasattr(self, k):
--- yawning_titan_gui/network_editor/cypress/e2e/pages/manage-networks-page.cy.ts
+++ yawning_titan_gui/network_editor/cypress/e2e/pages/manage-networks-page.cy.ts
@@ -2,4 +2,14 @@ describe('Manage Networks Page', () => {
   it('should open when the manage networks button is pressed', () => {
     cy.get('[data-cy="menu-manage-networks"]').click();
   });
+
+  describe('Make a new network', () => {
+    it('should create a network from scratch', () => {
+
+    });
+  });
+
+  describe('Make a copy of a network', () => {
+
+  });
 });
