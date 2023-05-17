2de4f81668596673131bf4975e1cde359388d425
Czar Echavez <Czar.Echavez@envitia.com>
2023-05-17 16:02:15 +0100

    #1400: run the server via waitress instead of flaskwebgui

--- src/yawning_titan_gui/management/commands/run_gui.py
+++ src/yawning_titan_gui/management/commands/run_gui.py
@@ -1,14 +1,130 @@
 import multiprocessing
-import os
 import subprocess
 import logging
 import sys
-from multiprocessing import Process
-from threading import Thread, Event
-from typing import Union
+import threading
 
 from django.core.management.base import BaseCommand
-from flaskwebgui import OPERATING_SYSTEM
+from yawning_titan_server.wsgi import application
+
+import os
+import time
+import signal
+import psutil
+import tempfile
+import platform
+import subprocess
+import socketserver
+import multiprocessing
+from multiprocessing import Process
+from threading import Thread
+from dataclasses import dataclass
+from typing import Callable, Any, Union
+
+OPERATING_SYSTEM = platform.system().lower()
+PY = "python3" if OPERATING_SYSTEM in ["linux", "darwin"] else "python"
+
+
+def get_free_port():
+    with socketserver.TCPServer(("localhost", 0), None) as s:
+        free_port = s.server_address[1]
+    return free_port
+
+class DefaultServerDjango:
+    @staticmethod
+    def get_server_kwargs(**kwargs):
+        return {"app": kwargs["app"], "port": kwargs["port"]}
+
+    @staticmethod
+    def server(**server_kwargs):
+        import waitress
+
+        waitress.serve(**server_kwargs)
+
+
+@dataclass
+class YawningTitanServer:
+    server: Union[str, Callable[[Any], None]]
+    server_kwargs: dict = None
+    app: Any = None
+    port: int = None
+    width: int = None
+    height: int = None
+    fullscreen: bool = True
+    on_startup: Callable = None
+    on_shutdown: Callable = None
+    socketio: Any = None
+
+    def __post_init__(self):
+        self.__keyboard_interrupt = False
+
+        if self.port is None:
+            self.port = (
+                self.server_kwargs.get("port")
+                if self.server_kwargs
+                else get_free_port()
+            )
+
+        if isinstance(self.server, str):
+            default_server = DefaultServerDjango
+            self.server = default_server.server
+            self.server_kwargs = self.server_kwargs or default_server.get_server_kwargs(
+                app=self.app, port=self.port, flask_socketio=self.socketio
+            )
+
+        self.profile_dir = os.path.join(tempfile.gettempdir(), "flaskwebgui")
+        self.url = f"http://127.0.0.1:{self.port}"
+
+    def _get_browser_process(self):
+        """Creates the thread used for the browser process"""
+        process = None
+        # check if windows
+        if sys.platform == 'win32':
+            process = subprocess.Popen(['start', self.url], shell=True)
+        # check if macOS
+        elif sys.platform == 'darwin':
+            process = subprocess.Popen(['open', self.url])
+        # catch all for other Operating Systems
+        else:
+            try:
+                process = subprocess.Popen(['xdg-open', self.url])
+            except OSError:
+                logging.warning('Unable to determine Operating System')
+                logging.warning('Please open a browser on: ' + self.url)
+
+        return process
+
+
+    def run(self):
+        end_server_event = threading.Event()
+
+        if self.on_startup is not None:
+            self.on_startup()
+
+        if OPERATING_SYSTEM == "darwin":
+            multiprocessing.set_start_method("fork")
+            server_process = Process(
+                target=self.server, kwargs=self.server_kwargs or {}
+            )
+        else:
+            server_process = Thread(target=self.server, kwargs=self.server_kwargs or {})
+
+        browser_thread = Thread(target=self._get_browser_process)
+
+        try:
+            # start server
+            server_process.start()
+            # start browser
+            browser_thread.start()
+
+            server_process.join()
+            browser_thread.join()
+
+        except KeyboardInterrupt:
+            self.__keyboard_interrupt = True
+            print("Stopped")
+
+        return server_process, browser_thread
 
 
 class Command(BaseCommand):
@@ -24,59 +140,12 @@ class Command(BaseCommand):
 
     def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
         super().__init__(stdout, stderr, no_color, force_color)
-        self.app = None
-
-    def __startup(self):
-        """The function called when starting up the GUI"""
-        # print the command used to run the gui
-        logging.log(logging.INFO, "Command: ".join(self.app.browser_command))
-
-    def __shutdown(self):
-        """The function called when the GUI is closed"""
-        logging.warning(f'Application has been shut down. {self.app.url} should no longer be up')
-
-    def __get_server_process(self):
-        """Creates the thread used for the server process"""
-        # check if os is macOS
-        if OPERATING_SYSTEM == "darwin":
-            multiprocessing.set_start_method("fork")
-            server_process = Process(
-                target=self.app.server, kwargs=self.app.server_kwargs or {}
-            )
-        else:
-            server_process = Thread(target=self.app.server, kwargs=self.app.server_kwargs or {})
-
-        # return the server process
-        return server_process
-
-    def __get_browser_process(self, server_process: Union[Thread, Process]):
-        """Creates the thread used for the browser process"""
-        process = None
-        # check if windows
-        if sys.platform == 'win32':
-            process = subprocess.Popen(['start', self.app.url], shell=True)
-        # check if macOS
-        elif sys.platform == 'darwin':
-            process = subprocess.Popen(['open', self.app.url])
-        # catch all for other Operating Systems
-        else:
-            try:
-                process = subprocess.Popen(['xdg-open', self.app.url])
-            except OSError:
-                logging.warning('Unable to determine Operating System')
-                logging.warning('Please open a browser on: ' + self.app.url)
-
-        return process
 
     def run(self):
         """
         Run the GUI backend server and then runs the GUI in the user's default browser
         """
-        from flaskwebgui import FlaskUI
-
-        from yawning_titan_server.wsgi import application
-
-        self.app = FlaskUI(
+        YawningTitanServer(  # noqa
             server="django",
             server_kwargs={
                 "app": application,
@@ -84,56 +153,7 @@ class Command(BaseCommand):
                 "host": "localhost"
             },
             fullscreen=True
-        )
-
-        logging.log(logging.DEBUG, self.app)
-
-        # run startup
-        self.__startup()
-
-        # setup server process
-        server_process = self.__get_server_process()
-
-        # setup browser thread
-        browser_thread = Thread(target=self.__get_browser_process, args=(server_process,))
-
-        # start threads
-        server_process.start()
-
-        # wait for server process to be online
-        while not server_process.is_alive():
-            os.wait()
-
-        try:
-            browser_thread.start()
-            server_process.join()
-            browser_thread.join()
-        except KeyboardInterrupt:
-            self.__keyboard_interrupt = True
-            print("Stopped")
-
-        # browser_thread.start()
-        #
-        # # join threads
-        # server_process.join()
-        # browser_thread.join()
-
-        # browser_active = True
-        #
-        # logging.log(logging.DEBUG, "server process: " + server_process.name)
-        # logging.log(logging.DEBUG, "browser process: " + browser_thread.name)
-        #
-        # while browser_active:
-        #     # check if the browser thread is still alive
-        #     browser_active = browser_thread.is_alive()
-        #     logging.log(logging.DEBUG, f"browser_active: {browser_active}")
-        #
-        #     if not browser_thread.is_alive():
-        #         # shutdown function
-        #         self.__shutdown()
-        #
-        #         # kill the server thread
-        #         server_process.kill()
+        ).run()
 
     def handle(self, *args, **kwargs):
         """Method that is fired on execution of the command in the terminal."""
