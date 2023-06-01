cfe5e34244c24ff71a239639cf333f7300965f52
Chris McCarthy <chris.mccarthy@methods.co.uk>
2023-06-01 17:16:18 +0100

    #1334 - Refactored setup_app_dirs.py and reset_default_notebooks.py to use app dirs from __init__.py following feedback from PR.
    Signed-off-by: Chris McCarthy <chris.mccarthy@methods.co.uk>

--- src/yawning_titan/__init__.py
+++ src/yawning_titan/__init__.py
@@ -21,35 +21,33 @@ Logging configured from the root:
 import logging.config
 import os
 import sys
-from pathlib import Path, PosixPath
+from pathlib import Path, PosixPath, WindowsPath
 from typing import Final, Union
 
 import yaml
 from gym.envs.registration import register
 from platformdirs import PlatformDirs
 
-register(id="five-node-def-v0", entry_point="yawning_titan.envs.specific:FiveNodeDef")
+register(id="five-node-def-v0",
+         entry_point="yawning_titan.envs.specific:FiveNodeDef")
 
-register(id="four-node-def-v0", entry_point="yawning_titan.envs.specific:FourNodeDef")
+register(id="four-node-def-v0",
+         entry_point="yawning_titan.envs.specific:FourNodeDef")
 
 register(
     id="networks-graph-explore-v0",
     entry_point="yawning_titan.envs.specific:GraphExplore",
 )
 
-register(id="18-node-env-v0", entry_point="yawning_titan.envs.specific:NodeEnv")
+register(id="18-node-env-v0",
+         entry_point="yawning_titan.envs.specific:NodeEnv")
 
 # Below handles application directories and user directories.
-# Uses `platformdirs.PlatformDirs` and `pathlib.Path` to create the required app directories in the correct locations
-# based on the users OS.
+# Uses `platformdirs.PlatformDirs` and `pathlib.Path` to create the required
+# app directories in the correct locations based on the users OS.
 
-_YT_ROOT_DIR: Final[Union[Path, PosixPath]] = Path(__file__).parent.resolve()
-
-_YT_PLATFORM_DIRS: Final[PlatformDirs] = PlatformDirs(appname="yawning_titan")
-"""An instance of `PlatformDirs` set with appname='yawning_titan'."""
-
-_YT_USER_DIRS: Final[Union[Path, PosixPath]] = Path.home() / "yawning_titan"
-"""The users home space for YT which is located at: ~/yawning_titan."""
+_YT_ROOT_DIR: Final[Union[Path, WindowsPath, PosixPath]] = Path(
+    __file__).parent.resolve()
 
 
 def _version() -> str:
@@ -60,75 +58,85 @@ def _version() -> str:
 
 __version__ = _version()
 
+_YT_PLATFORM_DIRS: Final[PlatformDirs] = PlatformDirs(appname="yawning_titan")
+"""An instance of `PlatformDirs` set with appname='yawning_titan'."""
 
-def _log_dir() -> Union[Path, PosixPath]:
+_YT_USER_DIRS: Final[
+    Union[Path, WindowsPath, PosixPath]] = Path.home() / "yawning_titan"
+"""The users home space for YT which is located at: ~/yawning_titan."""
+
+
+def _log_dir() -> Union[Path, WindowsPath, PosixPath]:
     if sys.platform == "win32":
         dir_path = _YT_PLATFORM_DIRS.user_data_path / "logs"
     else:
         dir_path = _YT_PLATFORM_DIRS.user_log_path
-    # Create if it doesn't already exist and bypass if it does already exist
-    dir_path.mkdir(parents=True, exist_ok=True)
     return dir_path
 
 
-# Force all to be created if not already
-DATA_DIR: Final[Union[Path, PosixPath]] = _YT_PLATFORM_DIRS.user_data_path
-"""The path to the app data directory as an instance of `Path` or `PosixPath`, depending on the OS."""
-
-
-LOG_DIR: Final[Union[Path, PosixPath]] = _log_dir()
+LOG_DIR: Final[Union[Path, WindowsPath, PosixPath]] = _log_dir()
 """The path to the app log directory as an instance of `Path` or `PosixPath`, depending on the OS."""
 
-
-DB_DIR: Final[Union[Path, PosixPath]] = _YT_PLATFORM_DIRS.user_data_path / "db"
+DB_DIR: Final[Union[
+    Path, WindowsPath, PosixPath]] = _YT_PLATFORM_DIRS.user_data_path / "db"
 """The path to the app db directory as an instance of `Path` or `PosixPath`, depending on the OS."""
 
-APP_IMAGES_DIR: Final[Union[Path, PosixPath]] = _YT_PLATFORM_DIRS.user_data_path / "app_images"
+APP_IMAGES_DIR: Final[
+    Union[
+        Path, WindowsPath, PosixPath]] = _YT_PLATFORM_DIRS.user_data_path / "app_images"
 """The path to the app images directory as an instance of `Path` or `PosixPath`, depending on the OS."""
 
-NOTEBOOKS_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "notebooks"
+NOTEBOOKS_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "notebooks"
 """
 The path to the users notebooks directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users notebooks are stored at: ~/yawning_titan/notebooks.
 """
 
-GAME_MODES_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "game_modes"
+GAME_MODES_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "game_modes"
 """
 The path to the users game modes directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users game modes are stored at: ~/yawning_titan/game_modes.
 """
 
-IMAGES_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "images"
+IMAGES_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "images"
 """
 The path to the users images directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users images are stored at: ~/yawning_titan/images.
 """
 
-VIDEOS_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "videos"
+VIDEOS_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "videos"
 """
 The path to the users videos directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users videos are stored at: ~/yawning_titan/videos.
 """
 
-AGENTS_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "agents"
+AGENTS_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "agents"
 """
 The path to the users agents directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users agents are stored at: ~/yawning_titan/agents.
 """
 
-AGENTS_LOGS_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "agents" / "logs"
+AGENTS_LOGS_DIR: Final[
+    Union[Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "agents" / "logs"
 """
 The path to the users agents logs directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
 Users agent logs are stored at: ~/yawning_titan/agents/logs.
 """
 
-PPO_TENSORBOARD_LOGS_DIR: Final[Union[Path, PosixPath]] = _YT_USER_DIRS / "agents" / "logs" / "tensorboard"
+PPO_TENSORBOARD_LOGS_DIR: Final[
+    Union[
+        Path, WindowsPath, PosixPath]] = _YT_USER_DIRS / "agents" / "logs" / "tensorboard"
 """
 The path to the PPO algorithm tensorboard logs directory as an instance of `Path` or `PosixPath`, depending on the OS.
 
@@ -137,7 +145,7 @@ Users agent PPO algorithm tensorboard logs are stored at: ~/yawning_titan/agents
 
 # Setup root logger format
 with open(
-    _YT_ROOT_DIR / "config" / "_package_data" / "logging_config.yaml", "r"
+        _YT_ROOT_DIR / "config" / "_package_data" / "logging_config.yaml", "r"
 ) as stream:
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
@@ -148,5 +156,5 @@ config["handlers"]["info_rotating_file_handler"]["filename"] = LOG_FILE_PATH
 
 try:
     logging.config.dictConfig(config)
-except Exception as e:
-    print(e)
+except Exception:  # noqa
+    pass
--- src/yawning_titan/utils/setup_app_dirs.py
+++ src/yawning_titan/utils/setup_app_dirs.py
@@ -1,7 +1,18 @@
 from logging import getLogger
-import yawning_titan  # noqa - Gets the Yawning-Titan logger config
-from logging import getLogger
 
+from yawning_titan import (
+    _YT_USER_DIRS,
+    PPO_TENSORBOARD_LOGS_DIR,
+    AGENTS_DIR,
+    AGENTS_LOGS_DIR,
+    VIDEOS_DIR,
+    IMAGES_DIR,
+    GAME_MODES_DIR,
+    NOTEBOOKS_DIR,
+    DB_DIR,
+    LOG_DIR,
+    APP_IMAGES_DIR
+)
 
 _LOGGER = getLogger(__name__)
 
@@ -13,44 +24,24 @@ def run():
     Uses `platformdirs.PlatformDirs` and `pathlib.Path` to create the required
     app directories in the correct locations based on the users OS.
     """
-    import sys
-    from pathlib import Path, PosixPath
-    from typing import Final, Union
+    app_dirs = [
+        _YT_USER_DIRS,
+        PPO_TENSORBOARD_LOGS_DIR,
+        AGENTS_DIR,
+        AGENTS_LOGS_DIR,
+        VIDEOS_DIR,
+        IMAGES_DIR,
+        GAME_MODES_DIR,
+        NOTEBOOKS_DIR,
+        DB_DIR,
+        LOG_DIR,
+        APP_IMAGES_DIR
+    ]
 
-    try:
-        from platformdirs import PlatformDirs
-
-        _YT_PLATFORM_DIRS: Final[PlatformDirs] = PlatformDirs(
-            appname="yawning_titan")
-        """An instance of `PlatformDirs` set with appname='yawning_titan'."""
-
-        app_dirs = [_YT_PLATFORM_DIRS.user_data_path]
-        if sys.platform == "win32":
-            app_dirs.append(_YT_PLATFORM_DIRS.user_data_path / "config")
-            app_dirs.append(_YT_PLATFORM_DIRS.user_data_path / "logs")
-            _YT_USER_DIRS: Final[
-                Union[Path, PosixPath]] = Path.home() / "yawning_titan"
-        else:
-            app_dirs.append(_YT_PLATFORM_DIRS.user_config_path)
-            app_dirs.append(_YT_PLATFORM_DIRS.user_log_path)
-            _YT_USER_DIRS: Final[
-                Union[Path, PosixPath]] = Path.home() / "yawning_titan"
-
-        app_dirs.append(_YT_PLATFORM_DIRS.user_data_path / "db")
-        app_dirs.append(_YT_PLATFORM_DIRS.user_data_path / "app_images")
-        app_dirs.append(_YT_USER_DIRS / "notebooks")
-        app_dirs.append(_YT_USER_DIRS / "game_modes")
-        app_dirs.append(_YT_USER_DIRS / "images")
-        app_dirs.append(_YT_USER_DIRS / "videos")
-        app_dirs.append(_YT_USER_DIRS / "agents")
-        app_dirs.append(_YT_USER_DIRS / "agents" / "logs" / "tensorboard")
-
-        for app_dir in app_dirs:
-            if not app_dir.is_dir():
-                app_dir.mkdir(parents=True, exist_ok=True)
-                _LOGGER.info(f"Created directory: {app_dir}")
-    except ImportError:
-        pass
+    for app_dir in app_dirs:
+        if not app_dir.is_dir():
+            app_dir.mkdir(parents=True, exist_ok=True)
+            _LOGGER.info(f"Created directory: {app_dir}")
 
 
 if __name__ == "__main__":
